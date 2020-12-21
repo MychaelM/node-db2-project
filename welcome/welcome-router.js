ae23bcdae9ca1aeebf63f9e68c50020c2e58b521
Mychael M <mychaelbmenges@gmail.com>
2020-12-20 17:01:17 -0700

    initial setup of server and knex init

---
+++ welcome/welcome-router.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.json({
+    message: "Hello from the server"
+  })
+})
+
+module.exports = router
